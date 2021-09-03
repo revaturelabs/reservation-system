3caa80baa383359b0b0d7d90491b6098dd275d31
Ashish <adepuashishkumar565@gmail.com>
2021-09-03 15:27:10 +0530

    adding new User

--- reservation-service/src/main/java/com/example/reservation/rest/UserController.java
+++ reservation-service/src/main/java/com/example/reservation/rest/UserController.java
@@ -1,7 +1,27 @@
 package com.example.reservation.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.reservation.model.User;
+import com.example.reservation.service.UserService;
+
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping
+    public ResponseEntity<?> post(@RequestBody User user) {
+        User newUser = userService.addNewUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+	
 }
--- reservation-service/src/main/java/com/example/reservation/service/UserServiceImpl.java
+++ reservation-service/src/main/java/com/example/reservation/service/UserServiceImpl.java
@@ -1,15 +1,30 @@
 package com.example.reservation.service;
 
+import com.example.reservation.exception.ResourceConflictExeception;
 import com.example.reservation.model.User;
+import com.example.reservation.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService{
-    @Override
-    public void addNewUser(User user) {
-
+    
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+    public User addNewUser(User user) {
+    	User newUser=null;
+        try {
+            newUser = userRepository.insert(user);
+        } catch (DuplicateKeyException e) {
+            throw new ResourceConflictExeception("user already exist");
+        }
+        return newUser;
     }
 
     @Override
