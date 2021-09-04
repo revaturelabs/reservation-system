3201aa2cd67347025dbcc72c411e8e9029052823
Ashish <adepuashishkumar565@gmail.com>
2021-09-04 18:35:33 +0530

    my changes

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -11,7 +11,6 @@ import { MatModule } from './mat.module'
 import { MatNativeDateModule } from '@angular/material/core';
 import { RouteTripsComponent } from './route-trips/route-trips.component';
 import { SeatsLayoutComponent } from './seats-layout/seats-layout.component';
-<<<<<<< HEAD
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegisterFormComponent } from './register-form/register-form.component'
 import { RouterModule,Routes } from '@angular/router'
@@ -23,12 +22,6 @@ const routes:Routes=[
 
 @NgModule({
   declarations: [AppComponent, SearchFormComponent, RouteTripsComponent, SeatsLayoutComponent, LoginFormComponent, RegisterFormComponent],
-=======
-import { TravellersComponent } from './travellers/travellers.component'
-
-@NgModule({
-  declarations: [AppComponent, SearchFormComponent, RouteTripsComponent, SeatsLayoutComponent, TravellersComponent],
->>>>>>> f67eda7c1a62acb76dfced1679721211236c6dbf
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
